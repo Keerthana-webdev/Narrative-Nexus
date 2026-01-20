8003db766b7b9ecaa56afabc02c334a4f776cb03
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-01-20 18:51:14 +0530

    committed changes

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-import streamlit as st
 from streamlit_ui import render_ui
-
-st.set_page_config(
-    page_title="NarrativeNexus",
-    layout="wide"
-)
 
 if __name__ == "__main__":
     render_ui()
